848b752e4637f8eb2350b5bc4d1642d343f13994
TJ <TJ@DESKTOP-J2LI010>
2020-05-25 11:01:56 +0900

    작성자: 윤형식
    common.js 네비 버튼 함수 수정
    common.js 페이지 버튼 함수 추가

--- holySite/WebContent/js/common.js
+++ holySite/WebContent/js/common.js
@@ -1,23 +1,33 @@
 function navBtnCngFnc(){
 	var navObj = '';
-	var locStr = location.pathname;
+	var locStr = location.pathname.split("/")[1];
 	
-	if (locStr == "/holySite/member/list" 
-		|| locStr == "/holySite/member/update" 
-		|| locStr == "/holySite/member/add") {
-		
+	if (locStr == "member") {
 		navObj = document.getElementById("navMember");
-		
-	} else if(locStr == "/holySite/bulletin/list"
-	 	|| locStr == "/holySite/bulletin/update"){
-		
+	} else if(locStr == "bulletin"){
 		navObj = document.getElementById("navBulletin");
-	} else if(locStr == "/holySite/notice/list"
-		|| locStr == "/holySite/notice/update"){
-		
+	} else if(locStr == "notice"){
 		navObj = document.getElementById("navNotice");
 	}
 
+	if(navObj != ''){
+		navObj.style.backgroundColor = "#FFFFFF";
+		navObj.style.color = "#59b1eb";
+	}
+}
+
+function pageBtnCngFnc(){
+	var navObjArr = document.getElementsByClassName("pageBtn");
+	var locInt = <%=request.getAttribute("pageNo")%>;
+	var navObj = '';
+	
+	for (var i = 0; i < navObjArr.length; i++) {
+		if(navObjArr[i].value == locInt){
+			navObj = navObjArr[i];
+			break;
+		}
+	}
+	
 	if(navObj != ''){
 		navObj.style.backgroundColor = "#FFFFFF";
 		navObj.style.color = "#59b1eb";
